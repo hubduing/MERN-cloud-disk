bf2242d04334d22862142472c7800248c9393782
Alexey B.V <ablogmount@gmail.com>
2022-01-22 22:07:59 +0500

    add token create and auth

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const Router = require('express');
 const User = require('../models/User');
 //  npm i bcryptjs
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 // npm i express-validator
-const {check, validationResult} = require('express-validator')
-const router = new Router()
+const {check, validationResult} = require('express-validator');
+const router = new Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // 1 параметр это эндпоинт
 // 2 параметр это массив для валидации email, pass
@@ -35,7 +37,7 @@ router.post('/registration',
     //  для безопастности пароль нужно захешировать
 
     // (это асинхронная операция)
-    const hashPassword = await bcrypt.hash(password, 15)
+    const hashPassword = await bcrypt.hash(password, 8)
 
     //  если нет пользователя в бд то создаем его
     const user = new User({email, password: hashPassword})
@@ -49,5 +51,42 @@ router.post('/registration',
     res.send({message: "Server error"})
   }
 })
+
+router.post('/login', async (req, res) => {
+    try{
+      const {email, password} = req.body
+      const user = await User.findOne({email})
+
+      if (!user) {
+        return res.status(404).json({message: "User not found"})
+      }
+      // если польователь найден сравниваем пароль
+      const isPassValid = bcrypt.compareSync(password, user.password)
+      // т.к. пароль хранится в бд в зшифрованном виде используем bcryptjs
+      if (!isPassValid) {
+        return res.status(400).json({message: "Invalid password"})
+      }
+      // устанавливаем npm i jsonwebtoken
+      // создаем токен. 1 параметр это объект с данными который мы хотим поместить в токен
+      // 2 секретный ключ (в конфиге) config/default.json
+      // 3 сколько токен будет существовать
+      const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'})
+      // возвращаем токен после создания на клиент
+      return res.json({
+        token,
+        user: {
+          id: user.id,
+          email: user.email,
+          diskSpace: user.diskSpace,
+          usedSpace: user.usedSpace,
+          avatar: user.avatar
+        }
+      })
+
+    } catch (e) {
+      console.log(e)
+      res.send({message: "Server error"})
+    }
+  })
 
 module.exports = router
