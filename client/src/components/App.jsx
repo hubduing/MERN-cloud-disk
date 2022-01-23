8541b56ee208aedbb9e82218ad59ba781295434b
Alexey B.V <ablogmount@gmail.com>
2022-01-23 11:08:23 +0500

    add reducer for client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect} from 'react';
+import './app.css'
+
+function App() {
+    return (
+        <div>Hello</div>
+    );
+}
+
+export default App;
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+
+const defaultState = {
+  currentuser: {},
+  isAuth: false
+}
+
+export default function userReducer(state = defaultState, action) {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
