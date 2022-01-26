d3f5c4c8e88b65e43537b85dd54df34cd3f83f7c
Alexey B.V <ablogmount@gmail.com>
2022-01-26 19:07:21 +0500

    Добавлен fileReducer (case & action). Добавлен редирект на главную и на логин для незарегистрированных

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,11 +4,12 @@
 import React, {useEffect} from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.css'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "../actions/user";
+import Disk from "./disk/Disk";
 
 function App() {
   const isAuth = useSelector(state => state.user.isAuth)
@@ -21,12 +22,18 @@ function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        <Navbar />
+        <Navbar/>
         <div className="wrap">
-          {!isAuth && 
+          {!isAuth ?
             <Switch>
-              <Route path="/login" component={Login} />
-              <Route path="/registration" component={Registration} />
+              <Route path="/login" component={Login}/>
+              <Route path="/registration" component={Registration}/>
+              <Redirect to="/ligon"/>
+            </Switch>
+            :
+            <Switch>
+              <Route exact path="/" component={Disk}/>
+              <Redirect to="/"/>
             </Switch>
           }
         </div>
